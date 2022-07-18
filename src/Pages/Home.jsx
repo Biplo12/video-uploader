75aa2fb4fb6cc3a416ee36be610a91f7ee80dfd8
Biplo12 <75808735+Biplo12@users.noreply.github.com>
2022-07-18 19:11:44 +0200

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Sidebar from "../Components/Sidebar/Sidebar";
+const Home = () => {
+  return (
+    <>
+      <Sidebar />
+    </>
+  );
+};
+
+export default Home;
